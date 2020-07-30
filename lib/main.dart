bc3a61f47af73a4eaa0c4b053517f7f4dd76abfa
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-07-30 21:22:46 +0100

    Add image and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,36 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/jean.jpg'),
+                radius: 50.0,
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Jean',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Nanum Gothic Coding',
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
